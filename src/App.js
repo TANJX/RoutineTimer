56b7c50438162f3478fafd550eb64cdd5bb0ceff
Mars Tan <jianxuat@usc.edu>
2022-06-06 20:24:51 +0800

    timer behavior

--- src/App.js
+++ src/App.js
@@ -1,36 +1,107 @@
 import './App.scss';
-import { useTimer } from 'react-timer-hook';
+import routine from './routine';
 import dayjs from 'dayjs';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
+const useAudio = (url) => {
+  const [audio] = useState(new Audio(url));
+  const [playing, setPlaying] = useState(false);
 
-const Timer = ({ endTime, duration }) => {
-  const {
-    seconds,
-    minutes,
-    isRunning,
-    pause,
-    resume,
-  } = useTimer({
-    expiryTimestamp: endTime,
-    onExpire: () => console.warn('onExpire called'),
+  const toggle = () => setPlaying(!playing);
+
+  useEffect(() => {
+    playing ? audio.play() : audio.pause();
+  }, [playing]);
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, []);
+
+  return [playing, toggle];
+};
+
+const useCountdown = (endTime) => {
+  const [countDown, setCountDown] = useState(
+    endTime - Date.now(),
+  );
+
+  useEffect(() => {
+    console.log(endTime, 'setInterval');
+    const interval = setInterval(() => {
+      const diff = endTime - Date.now();
+      if (diff < 0) {
+        setCountDown(0);
+        clearInterval(interval);
+      } else {
+        setCountDown(diff);
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, [endTime]);
+
+  return getReturnValues(countDown);
+};
+
+const getReturnValues = (countDown) => {
+  // calculate time left
+  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+  return { days, hours, minutes, seconds };
+};
+
+const Timer = ({ title, endTime, duration, finishedTime, totalDuration, onExpire }) => {
+  const [expire, setExpire] = useState(false);
+  const { minutes, seconds } = useCountdown(endTime);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setExpire(false);
+    }, 1000);
+  }, ([endTime]));
+
+  useEffect(() => {
+    document.title = title;
   });
 
-  let percent = 100 - (minutes * 60 + seconds) / duration * 100;
+  if (!expire && minutes + seconds <= 0) {
+    console.log('call expire');
+    setExpire(true);
+    onExpire();
+  }
+
+  const stateFinishedTime = (minutes * 60 + seconds);
+  let percent = 100 - stateFinishedTime / duration * 100;
   if (percent > 100) percent = 100;
-  console.log({ duration: Math.round(duration) + 's', end: dayjs(endTime).format("mm:ss") })
 
   const barStyle = {
     width: `${percent}%`,
   };
 
+  let totalPercent = (duration - stateFinishedTime + finishedTime) / totalDuration * 100;
+
+  const totalbarStyle = {
+    width: `${totalPercent}%`,
+  };
+
   return (
     <div className="timer">
+      <div className="totalprogress">
+        <div className='bar' style={totalbarStyle}></div>
+      </div>
       <div className='controls'>
-        <button onClick={isRunning ? pause : resume}>Resume</button>
+        {/* <button onClick={isRunning ? pause : resume}>Resume</button> */}
       </div>
       <div className='main'>
-        <p className='title'>1分钟 第1个</p>
+        <p className='title'>{title}</p>
         <p className='time'>
           <span className='minute'>{minutes}</span>
           <span className='colon'>:</span>
@@ -46,12 +117,37 @@ const Timer = ({ endTime, duration }) => {
 }
 
 function App() {
-  const [duration, setDuration] = useState(Math.random() * 100);
-  console.log({duration});
+  const [playing, playSound] = useAudio("n1.mp3");
+  const [routineState, setRoutineState] = useState(0);
+  const [startTime, setStartTime] =
+    useState(dayjs().add(routine[routineState].duration, 's').valueOf());
+
+  let totalTime = 0;
+  let finishedTime = 0;
+  for (let i = 0; i < routine.length; i++) {
+    if (i < routineState) finishedTime += routine[i].duration;
+    totalTime += routine[i].duration;
+  }
+
+  const handleExpire = () => {
+    // console.warn('expire');
+    playSound();
+    if (routineState + 1 < routine.length) {
+      setRoutineState(routineState + 1);
+      setStartTime(dayjs().add(routine[routineState + 1].duration, 's').valueOf());
+    }
+  }
+
   return (
     <div className="App">
-      <button onClick={() => { setDuration(Math.random() * 100) }}>change</button>
-      <Timer duration={duration} endTime={dayjs().add(duration, 's').valueOf()}></Timer>
+      <Timer
+        title={routine[routineState].title}
+        duration={routine[routineState].duration}
+        totalDuration={totalTime}
+        finishedTime={finishedTime}
+        endTime={startTime}
+        onExpire={handleExpire}
+      ></Timer>
     </div>
   );
 }
