3af9bb870855f54bd1524c94e98dc53273e02d7a
Mars Tan <jianxuat@usc.edu>
2022-06-06 14:18:54 +0800

    basic timer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import './App.scss';
+import { useTimer } from 'react-timer-hook';
+import dayjs from 'dayjs';
+import React, { useState } from 'react';
+
+
+const Timer = ({ endTime, duration }) => {
+  const {
+    seconds,
+    minutes,
+    isRunning,
+    pause,
+    resume,
+  } = useTimer({
+    expiryTimestamp: endTime,
+    onExpire: () => console.warn('onExpire called'),
+  });
+
+  let percent = 100 - (minutes * 60 + seconds) / duration * 100;
+  if (percent > 100) percent = 100;
+  console.log({ duration: Math.round(duration) + 's', end: dayjs(endTime).format("mm:ss") })
+
+  const barStyle = {
+    width: `${percent}%`,
+  };
+
+  return (
+    <div className="timer">
+      <div className='controls'>
+        <button onClick={isRunning ? pause : resume}>Resume</button>
+      </div>
+      <div className='main'>
+        <p className='title'>1分钟 第1个</p>
+        <p className='time'>
+          <span className='minute'>{minutes}</span>
+          <span className='colon'>:</span>
+          <span className='second'>{seconds < 10 ? `0${seconds}` : seconds}</span>
+        </p>
+      </div>
+      <div className="progress">
+        <div className='bar' style={barStyle}></div>
+      </div>
+    </div>
+
+  );
+}
 
 function App() {
+  const [duration, setDuration] = useState(Math.random() * 100);
+  console.log({duration});
   return (
     <div className="App">
-      <div className='main'>
-        <p className='title'>Title</p>
-        <p className='time'>00:30</p>
-      </div>
-      <div className="progress"></div>
+      <button onClick={() => { setDuration(Math.random() * 100) }}>change</button>
+      <Timer duration={duration} endTime={dayjs().add(duration, 's').valueOf()}></Timer>
     </div>
   );
 }
