6e0b4bda5692b026e46a596b31d0bc78b5812673
Mars Tan <jianxuat@usc.edu>
2022-06-06 11:32:25 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='main'>
+        <p className='title'>Title</p>
+        <p className='time'>00:30</p>
+      </div>
+      <div className="progress"></div>
+    </div>
+  );
+}
+
+export default App;
